19c3477414c476cc4a62df54fd431aaae4bb9726
jlin03 <jlin03@stuy.edu>
2018-10-15 19:28:06 -0400

    added realnumber and rationalnumber with empty methods

---
+++ RationalNumber.java
@@ -0,0 +1,22 @@
+public class RationalNumber extends RealNumber {
+	private double b;
+	
+	public RationalNumber() {
+		super(42);
+	}
+	
+	public RationalNumber(double in,double in2) {
+		super(in);
+		b = in2;
+	}
+	
+	public double getVal() {
+		return 0; 
+	}
+	
+	public static int compare(RationalNumber x, RationalNumber y) {
+		return 0;
+	}
+	
+	
+}
---
+++ RealNumber.java
@@ -0,0 +1,23 @@
+public class RealNumber {
+	private double a;
+	
+	public RealNumber(double in) {
+		a = in;
+	}
+	
+	public double getVal() {
+		return 0;
+	}
+	
+	public static int compare(RealNumber x, RealNumber y) {
+		return(0);
+	}
+	
+	public static int compare(RealNumber x, RationalNumber y) {
+		return(0);
+	}
+
+	
+	
+	
+}
