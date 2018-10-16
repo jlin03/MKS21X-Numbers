47da0005638f7bdd6f6b923177d7e435165c441a
jlin03 <jlin03@stuy.edu>
2018-10-16 10:15:09 -0400

    new methods added with some of them coded

--- RationalNumber.java
+++ RationalNumber.java
@@ -1,22 +1,58 @@
 public class RationalNumber extends RealNumber {
-	private double b;
-	
-	public RationalNumber() {
-		super(42);
-	}
-	
+	private int p,q;
+
 	public RationalNumber(double in,double in2) {
-		super(in);
-		b = in2;
+		super(0);
+		p = in;
+		q = in2;
 	}
-	
-	public double getVal() {
-		return 0; 
-	}
-	
-	public static int compare(RationalNumber x, RationalNumber y) {
-		return 0;
-	}
-	
-	
+
+	public double getValue(){
+    return (p/q);
+  }
+
+	public int getNumerator(){
+	 return p;
+ 	}
+
+ 	public int getDenominator(){
+	 	return q;
+ 	}
+
+	public RationalNumber multiply(RationalNumber other){
+    return(new RationalNumber(this.p * other.getNumerator(),this.q * other.getDenominator()));
+  }
+
+	public RationalNumber divide(RationalNumber other){
+	 return(new RationalNumber(multiply(reciprocal(other.getNumerator())),multiply(reciprocal(other.getDenominator()))));
+ 	}
+
+	public RationalNumber add(RationalNumber other){
+    return null;
+  }
+
+	public RationalNumber subtract(RationalNumber other){
+    return null;
+  }
+
+	public RationalNumber reciprocal(){
+    return(new RationalNumber(q,p));
+  }
+
+	private static int gcd(int a, int b){
+    return 0;
+  }
+
+	private void reduce(){
+
+  }
+
+	public boolean equals(RationalNumber other){
+    return false;
+  }
+
+	public String toString(){
+    return ("" + p + "/" + q);
+  }
+
 }
--- RealNumber.java
+++ RealNumber.java
@@ -1,23 +1,33 @@
 public class RealNumber {
 	private double a;
-	
+
 	public RealNumber(double in) {
 		a = in;
 	}
-	
-	public double getVal() {
-		return 0;
-	}
-	
-	public static int compare(RealNumber x, RealNumber y) {
-		return(0);
-	}
-	
-	public static int compare(RealNumber x, RationalNumber y) {
-		return(0);
+
+	public double getValue() {
+		return a;
 	}
 
-	
-	
-	
+	public double add(RealNumber other){
+    return(new RealNumber(this.getValue() + other.getValue()));
+  }
+
+	public double subtract(RealNumber other){
+    return(new RealNumber(this.getValue() - other.getValue()));
+  }
+
+	public double multiply(RealNumber other){
+    return(new RealNumber(this.getValue() * other.getValue()));
+  }
+
+	public double divide(RealNumber other){
+    return(new RealNumber(this.getValue() / other.getValue()));
+  }
+
+	public String toString(){
+    return ""+value;
+  }
+
+
 }
